b70297a59c3d55fcd8e72949910880a7edc19396
爱吃菠萝 <zhangjia_liang@foxmail.com>
2025-05-06 20:27:55 +0800

    Update main.py

--- google_scholar_crawler/main.py
+++ google_scholar_crawler/main.py
@@ -2,26 +2,31 @@ from scholarly import scholarly, ProxyGenerator
 import json
 from datetime import datetime
 import os
+from scholarly._proxy_generator import MaxTriesExceededException  # 导入异常类
 
 # Setup proxy
 pg = ProxyGenerator()
 pg.FreeProxies()  # Use free rotating proxies
 scholarly.use_proxy(pg)
 
-author: dict = scholarly.search_author_id(os.environ['GOOGLE_SCHOLAR_ID'])
-scholarly.fill(author, sections=['basics', 'indices', 'counts', 'publications'])
-name = author['name']
-author['updated'] = str(datetime.now())
-author['publications'] = {v['author_pub_id']:v for v in author['publications']}
-print(json.dumps(author, indent=2))
-os.makedirs('results', exist_ok=True)
-with open(f'results/gs_data.json', 'w') as outfile:
-    json.dump(author, outfile, ensure_ascii=False)
+try:
+    author: dict = scholarly.search_author_id(os.environ['GOOGLE_SCHOLAR_ID'])
+except MaxTriesExceededException as e:
+    print(f"发生异常: {e}，取消后续操作。")
+else:
+    scholarly.fill(author, sections=['basics', 'indices', 'counts', 'publications'])
+    name = author['name']
+    author['updated'] = str(datetime.now())
+    author['publications'] = {v['author_pub_id']:v for v in author['publications']}
+    print(json.dumps(author, indent=2))
+    os.makedirs('results', exist_ok=True)
+    with open(f'results/gs_data.json', 'w') as outfile:
+        json.dump(author, outfile, ensure_ascii=False)
 
-shieldio_data = {
-  "schemaVersion": 1,
-  "label": "citations",
-  "message": f"{author.get('citedby', 0)}",
-}
-with open(f'results/gs_data_shieldsio.json', 'w') as outfile:
-    json.dump(shieldio_data, outfile, ensure_ascii=False)
+    shieldio_data = {
+      "schemaVersion": 1,
+      "label": "citations",
+      "message": f"{author.get('citedby', 0)}",
+    }
+    with open(f'results/gs_data_shieldsio.json', 'w') as outfile:
+        json.dump(shieldio_data, outfile, ensure_ascii=False)
