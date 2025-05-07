858aabd4ef35607913f05a8ed65cd143c6bfe3d8
爱吃菠萝 <zhangjia_liang@foxmail.com>
2025-05-07 08:29:38 +0800

    Update main.py

--- google_scholar_crawler/main.py
+++ google_scholar_crawler/main.py
@@ -2,7 +2,7 @@ from scholarly import scholarly, ProxyGenerator
 import json
 from datetime import datetime
 import os
-from scholarly._proxy_generator import MaxTriesExceededException  # 导入异常类
+from scholarly._proxy_generator import MaxTriesExceededException
 
 # Setup proxy
 pg = ProxyGenerator()
@@ -13,6 +13,7 @@ scholarly.use_proxy(pg)
 try:
     print("正在创建结果目录...")
     os.makedirs('results', exist_ok=True)
+    
     print("正在查找作者信息...")
     author: dict = scholarly.search_author_id(os.environ['GOOGLE_SCHOLAR_ID'])
 except MaxTriesExceededException as e:
@@ -24,17 +25,20 @@ else:
     author['updated'] = str(datetime.now())
     author['publications'] = {v['author_pub_id']:v for v in author['publications']}
     print(json.dumps(author, indent=2))
+    
     print("正在保存作者数据...")
     with open(f'results/gs_data.json', 'w') as outfile:
         json.dump(author, outfile, ensure_ascii=False)
-
+        
     print("正在生成 Shields.io 数据...")
     shieldio_data = {
       "schemaVersion": 1,
       "label": "citations",
       "message": f"{author.get('citedby', 0)}",
     }
+    
     print("正在保存 Shields.io 数据...")
     with open(f'results/gs_data_shieldsio.json', 'w') as outfile:
         json.dump(shieldio_data, outfile, ensure_ascii=False)
+        
     print("数据处理完成。")
